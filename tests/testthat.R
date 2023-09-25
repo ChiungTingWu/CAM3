439c84607382c555e76f3ee9f6b65b0c70296715
Dongping Du <89655676+niccolodpdu@users.noreply.github.com>
2023-09-25 10:34:42 -0400

    Add files via upload

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(CAM3)
+
+test_check("CAM3")
---
+++ tests/testthat/test_CAM3.0.R
@@ -0,0 +1,23 @@
+context("Valid CAM3.0")
+test_that("Return NULL for inapplicable K", {
+    K <- 3
+    S <- matrix(2 ^ rnorm(1000*K, 5, 2.5), ncol = K)
+    # A <- matrix(c(0.2,0.4,0.6,0.8,
+    #               0.8,0.6,0.4,0.2), ncol = K)
+    A <- matrix(c(0.1,0.3,0.5,0.7,0.9,
+                  0.2,0.4,0.5,0.1,0.0,
+                  0.7,0.3,0.0,0.2,0.1), ncol = K)
+    X <- log2(A%*%t(S))  + rnorm(1000*nrow(A), 0 , 0.2)
+    X <- t (2 ^ X)
+
+    expect_warning(
+      CAMResult <- CAM3Run(X, K = 2:4, dim.rdc = 3,thres.low = 0.3,
+                          thres.high = 0.9, cluster.num = 20, MG.num.thres = 5)
+    )
+
+    expect_true(is.null(CAMResult@MGResult[[3]]))
+    expect_true(is.null(CAMResult@ASestResult[[3]]))
+
+    MDLres <- MDL(CAMResult)
+    expect_length(MDLres@mdls, 2)
+})
